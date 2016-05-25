2dca7b1cb0d4599ccb69c06c60ea052c3570fd69
krygu <kryguu@gmail.com>
2016-05-25 13:48:50 +0200

    Obstacles cd
    Reszta kodu, ktorej nie wrzucilem. Caly Form1.cs.

--- Driver/Form1.Designer.cs
+++ Driver/Form1.Designer.cs
@@ -29,11 +29,66 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Start = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Start);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(729, 490);
+            this.splitContainer1.SplitterDistance = 119;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(13, 13);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 490);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- Driver/Form1.cs
+++ Driver/Form1.cs
@@ -12,9 +12,48 @@ namespace Driver
 {
     public partial class Form1 : Form
     {
+        public Graphics canva { get; set; }
+        private List<Obstacle> _obstacles = new List<Obstacle> { };
+
         public Form1()
         {
             InitializeComponent();
+            canva = splitContainer1.Panel2.CreateGraphics();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Point point = new Point(400, 50);
+            Color color = Color.Black;
+            Obstacle obstacle = new Obstacle(point,color);
+            _obstacles.Add(obstacle);
+            _obstacles.Last().Field = canva;
+            paintObstacles();
+            timer.Enabled = true;
+        }
+
+        private void paintObstacles()
+        {
+            foreach (Obstacle obs in _obstacles)
+            {
+                obs.paint();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            splitContainer1.Panel2.Invalidate();
+            foreach (Obstacle obs in _obstacles)
+            {
+                obs.move();
+                obs.paint();
+            }
+            Invalidate();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+            paintObstacles();
         }
     }
 }
